47b8827b5f55e3a21c37f3aa387df153aca1f144
Sathya Mohan <sathya.mohan181986@gmail.com>
2026-03-31 15:26:20 +0530

    Updated tests folder

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,8 +1,17 @@
 import { test } from '@playwright/test';
 import { LoginActions } from '../src/actions/LoginActions';
+import { readExcelData } from '../src/utils/excelreader';
+import { LoginPage } from '../src/pages/LoginPage';
 
-test('Login Test', async ({ page }) => {
-  const login = new LoginActions(page);
+const testdata: any = readExcelData();
+//const baseData = testdata[0];
 
-  await login.login('rahulshettyacademy', 'learning');
-});
+test.describe('Rahul Shetty Academy', ()=> {
+  test('Login Test', async ({ page, context }) => {
+    const loginCheck = new LoginActions(page);
+    const loginUrl = testdata[0].pURL;
+    const Uname = testdata[0].pUserName;
+    const Pwd = testdata[0].pPassword;
+    await loginCheck.login(loginUrl, Uname, Pwd);
+})
+})
